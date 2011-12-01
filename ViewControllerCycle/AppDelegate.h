24fc188705d72c44d71a75e5a8ad5dcb6cba3e49
Ashley McCoy <ashey@mindeater.com>
2011-12-01 21:50:35 +1100

    Initial Commit

---
+++ ViewControllerCycle/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ViewControllerCycle
+//
+//  Created by Ashley McCoy on 1/12/11.
+//  Copyright (c) 2011 Mindeater Web Services. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ViewControllerCycle/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ViewControllerCycle
+//
+//  Created by Ashley McCoy on 1/12/11.
+//  Copyright (c) 2011 Mindeater Web Services. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
